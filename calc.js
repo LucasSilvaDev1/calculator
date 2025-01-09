3f512787e38c37e01d132ab55676ed4f95fbb284
Lucas silva <134072600+LucasSilvaDev1@users.noreply.github.com>
2025-01-09 13:21:10 -0300

    botão de expandir configurado

--- calc.js
+++ calc.js
@@ -34,13 +34,65 @@ function backspace() {
         display.textContent = "0";
     }
 }
+
+function toggleClasses(element, classList) {
+    classList.forEach(_class => {
+        element.classList.toggle(_class)
+    });
+}
+
 function exibirBtns() {
     const btn = document.getElementById('addbutton');
-    const btns = document.querySelectorAll(".calcsAdicionais");
+    const btns = document.querySelectorAll(".btn");
+    const display = document.querySelector("#display")
+    const calculadora = document.querySelector("#calculadora")
+    const btns2 = document.querySelector("#btns")
+   
+    toggleClasses(display, ["display", "display2"])
+    toggleClasses(calculadora, ["calculadora", "calculadora2"])
+    toggleClasses(btns2, ["btns", "btns2"])
 
-    btn.addEventListener("click", () =>{
-        btns.forEach(button =>{
-           button.style.display = button.style.display === "none" ? "block" : "none";
-        })
-    })
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].classList.toggle("calcsAdicionais")
+    }
+    // classes.forEach(element => {
+    //     console.log(element)
+    //     display.classList.toggle(element)
+    //     calculadora.classList.toggle(element)
+    //     btns2.classList.toggle(element)
+    // });
+    // display.classList.toggle("display")
+    // display.classList.toggle("display2")
+
+    // calculadora.classList.toggle("calculadora")
+    // calculadora.classList.toggle("calculadora2")
+
+    // btns2.classList.toggle("btns")
+    // btns2.classList.toggle("btns2")
+
+
+    // if (display.classList.contains("display")) {
+    //     display.classList.remove("display")
+    //     display.classList.add("display2")
+    // } else {
+    //     display.classList.remove("display2")
+    //     display.classList.add("display")
+    // }
+    // if (calculadora.classList.contains("calculadora")) {
+    //     calculadora.classList.remove("calculadora")
+    //     calculadora.classList.add("calculadora2")
+    // } else {
+    //     calculadora.classList.remove("calculadora2")
+    //     calculadora.classList.add("calculadora")
+    // }
+    // if (btns2.classList.contains("btns")) {
+    //     btns2.classList.remove("btns")
+    //     btns2.classList.add("btns2")
+    // } else {
+    //     btns2.classList.remove("btns2")
+    //     btns2.classList.add("btns")
+    // }
+    
+
+    
 }
